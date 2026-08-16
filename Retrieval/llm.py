3753074c28388bb75a86ede252454829388861b1
owais-js <owaisshabbir3131@gmail.com>
2026-08-16 16:28:56 +0500

    optimize llm instance reuse

--- Retrieval/llm.py
+++ Retrieval/llm.py
@@ -3,16 +3,27 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+
 load_dotenv()
 
 
+_llm = None
+
+
 def get_llm():
-    api_key = os.getenv("GOOGLE_API_KEY")
 
-    if not api_key:
-        raise ValueError("GOOGLE_API_KEY not found.")
+    global _llm
 
-    return ChatGoogleGenerativeAI(
-        model="gemini-3.5-flash",
-        temperature=0
-    )
+    if _llm is None:
+
+        api_key = os.getenv("GOOGLE_API_KEY")
+
+        if not api_key:
+            raise ValueError("GOOGLE_API_KEY not found.")
+
+        _llm = ChatGoogleGenerativeAI(
+            model="gemini-3.5-flash",
+            temperature=0
+        )
+
+    return _llm
